9f71629180d7d2fea8af9ebdb8c9f47cf329de71
Ahmad <rabiu2993@gmail.com>
2025-10-18 15:55:09 +0100

    feat(#10): add the plicense lets you preview and add licenses to your project using an interactive terminal UI.
    Usage:
      plicense [flags]
      plicense [command]
    
    Available Commands:
      add         Interactively choose and add a license to your project
      completion  Generate the autocompletion script for the specified shell
      delete      Delete your project license directly from the CLI
      help        Help about any command
    
    Flags:
      -h, --help   help for plicense
    
    Use "plicense [command] --help" for more information about a command. flag handler

--- internals/cli/root.go
+++ internals/cli/root.go
@@ -8,23 +8,59 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var showHelp bool
+
 var rootCmd = &cobra.Command{
 	Use:     "plicense",
 	Short:   "Add open-source licenses to your project interactively",
-	Long:    "plicense lets you preview and add licenses to your project using an interactive terminal UI.",
+	Long:    "plicense lets you preview, add, and manage licenses for your projects using an interactive terminal UI.",
 	Version: Version,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("%s", color.HiBlueString("\nName: pLICENSE\n"))
 		fmt.Printf("Version: v%s\n", color.HiYellowString(Version))
-		fmt.Printf("Commit: %s\n", Commit)
-		fmt.Printf("Built at: %s\n", Date)
-		fmt.Printf("%s", color.HiCyanString("Run 'plicense --help' to see the list of commands\n"))
+		fmt.Printf("Commit: %s\n", color.HiMagentaString(Commit))
+		fmt.Printf("Built at: %s\n", color.HiCyanString(Date))
+		fmt.Printf("%s", color.HiCyanString("\nRun 'plicense --help' to see the list of commands.\n"))
 	},
 }
 
+func init() {
+	rootCmd.SetHelpTemplate(customHelpTemplate)
+}
+
 func Execute() {
+	rootCmd.Flags().BoolVarP(&showHelp, "help", "h", false, "Show help details for plicense")
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
+
+var customHelpTemplate = fmt.Sprintf(`
+%s
+Usage: {{.UseLine}}
+
+{{.Long}}
+
+%s
+   add         Add open-source licenses interactively or by key
+   delete      Delete the LICENSE file from the current directory
+   view        View the contents of your project’s license
+
+%s
+   version     Show the installed version of plicense
+   help        Show this help message
+
+Examples:
+   plicense add
+   plicense add mit
+   plicense delete
+   plicense view
+   plicense version
+
+Run 'plicense help <command>' for details on a specific command.
+`,
+	color.HiCyanString("These are common pLICENSE commands used in various situations:"),
+	color.HiYellowString("Manage licenses for your project"),
+	color.HiYellowString("Get information about plicense"),
+)
